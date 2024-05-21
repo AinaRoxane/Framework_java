87ca061b3f537c70acbf2c2434d3be0334c4f1ff
AinaRoxane <roxane.rakotoarimanana@gmail.com>
2024-05-21 10:29:08 +0200

    sprint2

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -1,18 +1,39 @@
 package mg.itu.prom16;
-
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+
+import annotation.Controller;
+import annotation.Get;
+import utils.Mapping;
+import utils.PackageScanner;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontController extends HttpServlet {
+        PackageScanner scanner;
+        HashMap<String , Mapping > ListService;
+
+    public void init() {
+        scanner = new PackageScanner();
+        // get parameter written in web.xml:
+        String packagename = this.getInitParameter("package");
+        // get all controller within packagename:
+        ListService = scanner.getMapping(packagename, Controller.class);
+    }
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            out.println("Hello, url =" + request.getRequestURL());
-        }
+        PrintWriter out = response.getWriter();
+            String url = scanner.conform_url(request.getRequestURL().toString());
+            try {
+                Mapping mapping = ListService.get(url);
+                out.println("url: "+url+" | className: "+mapping.getClassName()+" , methodName: "+mapping.getMethodName());
+            } catch (NullPointerException e) {
+                out.println("Aucun controller ne prend en compte cet url!");
+            }
     }
 
     @Override
@@ -25,3 +46,4 @@ public class FrontController extends HttpServlet {
         processRequest(request, response);
     }
 }
+
