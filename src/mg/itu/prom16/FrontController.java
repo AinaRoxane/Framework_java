df14e872af8794f6694300c49dd7b586250ba1bf
AinaRoxane <roxane.rakotoarimanana@gmail.com>
2024-06-18 13:34:22 +0300

    sprint7-2483: envoyer des objets  via un formulaire

--- src/mg/itu/prom16/FrontController.java
+++ src/mg/itu/prom16/FrontController.java
@@ -5,8 +5,10 @@ import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import annotation.Controller;
 import annotation.Get;
@@ -51,45 +53,72 @@ public class FrontController extends HttpServlet {
 
         try {
             Class<?> clazz = Class.forName(mapping.getClassName());
+            // Controller instance:
             Object instance = clazz.getDeclaredConstructor().newInstance();
-            Method method = clazz.getDeclaredMethod(mapping.getMethodName());
-
-            // Prepare method arguments
-            List<Object> methodArgs = new ArrayList<>();
-            String paramName = "";
-            for(Parameter parameter : method.getParameters()){
-                if(parameter.isAnnotationPresent(Param.class)){
-                    paramName = parameter.getAnnotation(Param.class).name();
-                } else{
-                    paramName = parameter.getName();
+            // get matching method for url
+            Method method = null;
+            for (Method m : clazz.getDeclaredMethods()) {
+                if (m.getName().equals(mapping.getMethodName())) {
+                    method = m;
+                    break;
                 }
-                String paramValue = request.getParameter(paramName);
-                methodArgs.add(Convertor.convertParameter(parameter.getType(), paramValue));
-                
             }
 
-            Object result = method.invoke(instance, methodArgs.toArray());
+            // get All the parameters name from a form:
+            Enumeration<String> parameterNames = request.getParameterNames();
+            // Map to hold created objects
+            Map<String, Object> objets = new HashMap<String, Object>();
+            while (parameterNames.hasMoreElements()) {
+                String paramName = parameterNames.nextElement();
+                String[] parts = paramName.split("\\.");
+                if (parts.length > 1) {
+                    String objectName = parts[0];
+                    Class<?> objetclazz = Class.forName("model." + objectName);
+                    Object mydataholder;
+                    if (!objets.containsKey(objectName)) {
+                        mydataholder = objetclazz.getDeclaredConstructor().newInstance();
+                        objets.put(objectName, mydataholder);
+                    } else {
+                        mydataholder = objets.get(objectName);
+                    }
+                    Method datasetter = null;
+                    for (Method m : objetclazz.getDeclaredMethods()) {
+                        if (m.getName().equals("set" + parts[1].substring(0, 1).toUpperCase() + parts[1].substring(1))) {
+                            datasetter = m;
+                            break;
+                        }
+                    }
+                    String paramValue = request.getParameter(paramName);
+                    datasetter.invoke(mydataholder, Convertor.convertParameter(datasetter.getParameterTypes()[0], paramValue));
+                } else {
+                    String paramValue = request.getParameter(paramName);
+                    objets.put(paramName, paramValue);
+                }
+            }
 
+            // Prepare method arguments:
+            List<Object> methodArgs = new ArrayList<>();
+            for (Parameter parameter : method.getParameters()) {
+                String paramName = parameter.isAnnotationPresent(Param.class) ? parameter.getAnnotation(Param.class).name() : parameter.getName();
+                methodArgs.add(objets.get(paramName));
+            }
+
+            // Invoke the controller method
+            Object result = method.invoke(instance, methodArgs.toArray());
             if (result instanceof String) {
                 out.println(result);
-            }
-            else if (result instanceof ModelView) {
+            } else if (result instanceof ModelView) {
                 ModelView mv = (ModelView) result;
-                request.setAttribute(mv.getVariableName(), mv.getData().get(mv.getVariableName()));
+                mv.getData().forEach(request::setAttribute);
                 RequestDispatcher dispatcher = request.getRequestDispatcher(mv.getUrl());
                 dispatcher.forward(request, response);
-            } else{
-                throw new ReturnTypeException("Return type no handled for :"+url);
+            } else {
+                throw new ReturnTypeException("Return type not handled for: " + url);
             }
-        } catch (ClassNotFoundException | NoSuchMethodException | SecurityException | InstantiationException
-                | IllegalAccessException | IllegalArgumentException | ReturnTypeException e) {
+        } catch (Exception e) {
             out.println("<h3>Oops!</h3>");
             out.println("<p>An error occurred while processing the request.</p>");
             out.println(e);
-        } catch (Exception e) {
-            out.println("<h3>Oops!</h3>");
-            out.println("<p>An unexpected error occurred.</p>");
-            out.println(e);;
         }
     }
 
